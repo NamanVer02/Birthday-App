20ef7c28e79837aaaec267ae07f800e6667a5528
Naman Verma <82778097+NamanVer02@users.noreply.github.com>
2022-01-24 15:30:15 +0530

    Update and rename BirthdayApp.py to Kivy_UI.py

--- Kivy_UI.py
+++ Kivy_UI.py
@@ -4,7 +4,7 @@ from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-Builder.load_file('AddBirthdays.kv')
+Builder.load_file('Kivy_Design.kv')
 Window.size = (350, 600)
 
 class TheLayout(Widget):
