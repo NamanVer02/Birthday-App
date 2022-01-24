5f0eba0f304b7dabb1e8f49d4780b39e40f64d62
Naman Verma <82778097+NamanVer02@users.noreply.github.com>
2022-01-24 15:29:40 +0530

    Add files via upload

---
+++ BirthdayApp.py
@@ -0,0 +1,18 @@
+import imp
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+Builder.load_file('AddBirthdays.kv')
+Window.size = (350, 600)
+
+class TheLayout(Widget):
+        pass
+
+class TheApp(App):
+    def build(self):
+        return TheLayout()
+
+if __name__ == '__main__':
+    TheApp().run()
