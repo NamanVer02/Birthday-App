d1796252219651c4d83b7cc89507c1d3596c1ae6
Naman Verma <82778097+NamanVer02@users.noreply.github.com>
2022-01-25 14:46:01 +0530

    Update Kivy_UI.py

--- Kivy_UI.py
+++ Kivy_UI.py
@@ -1,18 +1,40 @@
-import imp
+import pickle
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-Builder.load_file('Kivy_Design.kv')
+Builder.load_file('AddBirthdays.kv')
 Window.size = (350, 600)
 
+
+try:
+    data = pickle.load(open('data.dat', 'rb'))
+except FileNotFoundError:
+    data = []
+
+
 class TheLayout(Widget):
-        pass
+    pass
+
 
 class TheApp(App):
     def build(self):
         return TheLayout()
+
+    
+    def Add(self):
+        first_name = self.root.ids.first_name.text
+        last_name = self.root.ids.last_name.text
+        nickname = self.root.ids.nickname.text
+        phone = self.root.ids.phone.text
+        birthday = self.root.ids.birthday.text
+
+        data.append([first_name+last_name, nickname, phone, birthday])
+        print(data)
+        pickle.dump(data, open('data.dat', 'wb'))
+
+
 
 if __name__ == '__main__':
     TheApp().run()
